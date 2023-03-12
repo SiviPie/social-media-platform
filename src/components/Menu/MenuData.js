fd4159dcc5e6f67a89741ffab3d21ab07f1628e7
sivipie <sivipie@yahoo.com>
2023-03-12 12:37:44 +0200

    added routes

---
+++ src/components/Menu/MenuData.js
@@ -0,0 +1,17 @@
+export const MenuData =  [
+    {
+        title: "Home",
+        path: "/",
+        cName: "nav-text" //className
+    },
+    {
+        title: "About",
+        path: "/about",
+        cName: "nav-text" 
+    },
+    {
+        title: "Contact",
+        path: "/contact",
+        cName: "nav-text" 
+    },
+]
--- src/index.js
+++ src/index.js
@@ -1,14 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+
+import Menu from "./components/Menu/Menu";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Footer from "./components/Footer/Footer";
+
+const AppLayout = () => (
+  <>
+    <Menu />
+    <Outlet />
+    <Footer />
+  </>
+);
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
 );
 
 // If you want to start measuring performance in your app, pass a function
